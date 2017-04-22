7ae843c5c1d99f7e6db073c2a02430aa3854a8c2
campbellyamane <campbellyamane@gmail.com>
2017-04-22 14:18:39 -0700

    Update authorize.php

--- php/authorize.php
+++ php/authorize.php
@@ -2,7 +2,7 @@
 
 	require 'vendor/autoload.php';
 
-	$session = new SpotifyWebAPI\Session('bafab0def2814af48f3cadf80c42ae17', 'd5c6f92f78704bb8abbe7f82a8ff3fc1', 'http://localhost/spotme/php/home.php');
+	$session = new SpotifyWebAPI\Session('id', 'password', 'http://localhost/spotme/php/home.php');
 
 	$scopes = array(
 			'playlist-read-private',
